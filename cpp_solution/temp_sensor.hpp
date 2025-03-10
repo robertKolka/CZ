b774901fd05f0f942404e57d63367fbd57cd624e
Robert <rob783@gmail.com>
2025-03-10 16:30:40 +0100

    added temp_sensor.hpp

---
+++ cpp_solution/temp_sensor.hpp
@@ -0,0 +1,37 @@
+/* 
+   This header file contains example structs and definitions
+   for demonstrating how µC pins might be set for a given microcontroller.
+   It will be usually provided by µC vendor libraries
+*/
+
+#define GPIO_IN  0
+#define GPIO_OUT 1
+#define I2C_SDA 2
+#define I2C_SCL 2
+#define ADC 2
+
+#define GPIO_LOW 0
+#define GPIO_HIGH 1
+
+struct µC_pins {
+    int pin1;
+    int pin2;
+    int pin3;
+    int pin4;
+    int pin5;
+    int pin6;
+};
+
+struct set_GPIO {
+    int pin1;
+    int pin2;
+    int pin3;
+    int pin4;
+    int pin5;
+    int pin6;
+
+};
+
+struct cpu1_timer {
+    int cpu1_timer_period;
+};
