d02eb206838edfc0896daf0a3cc6b21f6e48cf29
Robert <rob783@gmail.com>
2025-03-10 07:50:23 +0100

    Added example header file temp_sensor.h and pin settings for LEDs, EEPROM and ADC

--- c_solution/temp_sensor.c
+++ c_solution/temp_sensor.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
+#include "temp_sensor.h"
+
+
+void set_LED_pins() {
+    // set pins 1, 2 and 3 to GPIO
+    pinsUseCaseTemp.pin1 = GPIO_OUT; // red
+    pinsUseCaseTemp.pin2 = GPIO_OUT; // yellow
+    pinsUseCaseTemp.pin3 = GPIO_OUT; // green
+}
+
+void set_EEPROM_pins() {
+    // set pins 4 and 5 to I2C
+    pinsUseCaseTemp.pin4 = I2C_SCL;
+    pinsUseCaseTemp.pin5 = I2C_SDA;
+}
+
+void set_ADC_pin() {
+    // set pins 4 and 5 to I2C
+    pinsUseCaseTemp.pin6 = ADC;
+}
+
+void set_red_LED() {
+    // red LED should turn on, others should turn off
+    setGPIO_t.pin1 = GPIO_LOW;
+    setGPIO_t.pin2 = GPIO_HIGH;
+    setGPIO_t.pin3 = GPIO_HIGH;
+}
+void set_yellow_LED() {
+    // yellow LED should turn on, others should turn off
+    setGPIO_t.pin1 = GPIO_HIGH;
+    setGPIO_t.pin2 = GPIO_LOW;
+    setGPIO_t.pin3 = GPIO_HIGH;
+}
+void set_green_LED() {
+    // green LED should turn on, others should turn off
+    setGPIO_t.pin1 = GPIO_HIGH;
+    setGPIO_t.pin2 = GPIO_HIGH;
+    setGPIO_t.pin3 = GPIO_LOW;
+}
 
 int main() {
-
-    // Writing print statement to print hello world
-    printf("Hello World");
+    printf("Running temp_sensor.c");
+    set_LED_pins();
+    set_EEPROM_pins();
+    set_ADC_pin();
 
     return 0;
 }
---
+++ c_solution/temp_sensor.h
@@ -0,0 +1,34 @@
+/* this header file contains example structs and definitions
+   for demonstrating how µC pins might be set.
+   commonly provided by µC vendor libraries
+*/
+
+#define GPIO_IN  0
+#define GPIO_OUT 1
+#define I2C_SDA 2
+#define I2C_SCL 2
+#define ADC 2
+
+#define GPIO_LOW 0
+#define GPIO_HIGH 1
+
+struct µC_pins {
+    int pin1;
+    int pin2;
+    int pin3;
+    int pin4;
+    int pin5;
+    int pin6;
+};
+
+struct set_GPIO {
+    int pin1;
+    int pin2;
+    int pin3;
+    int pin4;
+    int pin5;
+    int pin6;
+};
+
+struct µC_pins pinsUseCaseTemp;
+struct set_GPIO setGPIO_t;
